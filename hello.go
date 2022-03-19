80f5d369e630739b941c989177267078a8086b90
shun0305 <shunbbra@icloud.com>
2022-03-19 15:11:42 +0900

    stacking defer

--- hello.go
+++ hello.go
@@ -1,21 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("When's Saturday?")
-	today := time.Now().Weekday()
-	switch time.Saturday {
-	case today + 0:
-		fmt.Println("Today.")
-	case today + 1:
-		fmt.Println("Tomorrow.")
-	case today + 2:
-		fmt.Println("In two days.")
-	default:
-		fmt.Println("Too far away.")
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
 	}
+
+	fmt.Println("done")
 }
