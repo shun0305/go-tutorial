3a4ec50f4808ebd6abc7eb1b08eac8827988123b
shun0305 <shunbbra@icloud.com>
2022-03-19 11:59:44 +0900

    type

--- hello.go
+++ hello.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
+func add(x int, y int) int {
+	return x + y
+}
+
+// 型省略
+// func add(x , y int) int {
+// 	return x + y
+// }
+
+func swap(x, y string) (string, string) {
+	return x, y
+}
+
 func main() {
-	fmt.Printf("Now you have %g problems.\n", math.Sqrt(9))
-	fmt.Println(math.Pi)
+	// fmt.Println(add(42, 13))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 }
