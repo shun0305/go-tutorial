bd487bf3704d488eb538c20ce1624d11a0ec898d
shun0305 <shunbbra@icloud.com>
2022-03-19 10:09:55 +0900

    math

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(9))
+	fmt.Println(math.Pi)
 }
