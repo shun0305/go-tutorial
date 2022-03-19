7f75ec9989dde9a89e6468d2eb8a56fd994cb409
shun0305 <shunbbra@icloud.com>
2022-03-19 12:41:47 +0900

    naked return

--- hello.go
+++ hello.go
@@ -4,21 +4,12 @@ import (
 	"fmt"
 )
 
-func add(x int, y int) int {
-	return x + y
-}
-
-// 型省略
-// func add(x , y int) int {
-// 	return x + y
-// }
-
-func swap(x, y string) (string, string) {
-	return x, y
+func split(some int) (x, y int) {
+	x = some * 4 / 9
+	y = some - x
+	return
 }
 
 func main() {
-	// fmt.Println(add(42, 13))
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
+	fmt.Println(split(17))
 }
