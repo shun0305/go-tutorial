cd09c27e3cca3f457c9714652d0560e9b95895ac
shun0305 <shunbbra@icloud.com>
2022-03-22 08:35:30 +0900

    slice

--- hello.go
+++ hello.go
@@ -2,18 +2,21 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	X int
-	Y int
+func main() {
+	a := make([]int, 5)
+	printSlice("a", a)
+
+	b := make([]int, 0, 5)
+	printSlice("b", b)
+
+	c := b[:2]
+	printSlice("c", c)
+
+	d := c[2:5]
+	printSlice("d", d)
 }
 
-var (
-	v1 = Vertex{1, 2}  // has type Vertex
-	v2 = Vertex{X: 1}  // Y:0 is implicit
-	v3 = Vertex{}      // X:0 and Y:0
-	p  = &Vertex{1, 2} // has type *Vertex
-)
-
-func main() {
-	fmt.Println(v1, p, v2, v3)
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
 }
